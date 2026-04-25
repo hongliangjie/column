9a8651ddb3f63ca864da3f9d5fab33dda4ba552e
LIANGJIE HONG <hongliangjie@gmail.com>
2026-04-25 16:09:13 -0700

    changes

--- _site/run.sh
+++ _site/run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+export SDKROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.1.sdk
+export LIBRARY_PATH=/usr/local/lib
 bundle exec jekyll serve
